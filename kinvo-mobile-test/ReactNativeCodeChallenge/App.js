cd34df3e7b9667ca610db68138d2d26ee83e12a0
Daniel Hermenegildo <danielmarcosh@gmail.com>
2021-02-07 20:59:03 -0300

    Configurando React Navigation

--- kinvo-mobile-test/ReactNativeCodeChallenge/App.js
+++ kinvo-mobile-test/ReactNativeCodeChallenge/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
+function Home() {
   return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.welcome}>Criação do Projeto</Text>
-      </SafeAreaView>
-    </>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Bem vindo!</Text>
+    </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{title: 'Home'}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
