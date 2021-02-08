ec952e93f1bf4615dfaa58767d63168e39b12bd0
Daniel Hermenegildo <danielmarcosh@gmail.com>
2021-02-08 05:31:29 -0300

    Tela desafio - Acoes

--- kinvo-mobile-test/ReactNativeCodeChallenge/src/pages/Home/index.js
+++ kinvo-mobile-test/ReactNativeCodeChallenge/src/pages/Home/index.js
@@ -1,84 +1,52 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
-  FlatList,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
+  View,
   Text,
+  StyleSheet,
+  StatusBar,
   TouchableOpacity,
 } from 'react-native';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Ações',
-    description: 'Nacionais',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Fundos',
-    description: 'De investimentos',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Previdências',
-    description: 'Privadas',
-  },
-];
-
-const Item = ({item, onPress, style}) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
-    <Text style={styles.title}>{item.title}</Text>
-    <Text style={styles.description}>{item.description}</Text>
-  </TouchableOpacity>
-);
-
 function Home({navigation}) {
-  const [selectedId] = useState(null);
-  let route;
-
-  if (selectedId === 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba') {
-    route = 'Acoes';
-  } else if (selectedId === '3ac68afc-c605-48d3-a4f8-fbd91aa97f63') {
-    route = 'Fundos';
-  } else if (selectedId === '58694a0f-3da1-471f-bd96-145571e29d72') {
-    route = 'Previdencia';
-  } else {
-    route = 'Home';
-  }
-  console.log(route)
-  const renderItem = ({item}) => {
-    return (
-      <Item
-        item={item}
-        onPress={() => navigation.navigate(route)}
-        style={{backgroundColor: '#ECF0F2'}}
-      />
-    );
-  };
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('Acoes')}>
+        <View style={styles.item}>
+          <Text style={styles.title}>Ações</Text>
+          <Text style={styles.description}>Status</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#ECF0F2',
+    fontFamily: 'Montserrat',
   },
   item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingLeft: 20,
+    marginVertical: 20,
+    marginHorizontal: 20,
+    height: 120,
+    width: 365,
+    backgroundColor: '#FFFFFF',
+    borderColor: '#707070',
+    borderWidth: 0.5,
+    borderRadius: 10,
   },
   title: {
-    fontSize: 32,
+    fontSize: 18,
+    color: '#6F4DBF',
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 12,
+    color: '#627179',
   },
 });
 
